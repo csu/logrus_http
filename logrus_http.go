9c4a9148b7c9b1df8eebc6165afff721038e9aa8
Christopher Su <christophersu9@gmail.com>
2016-02-28 07:29:31 -0800

    working

---
+++ logrus_http.go
@@ -0,0 +1,52 @@
+package logrus_http
+
+import (
+  "fmt"
+  "github.com/Sirupsen/logrus"
+  "net/http"
+  "net/url"
+  "os"
+)
+
+type HttpHook struct {
+  RequestEndpoint string
+  RequestFormKey string
+  RequestExtraFields map[string]string
+}
+
+// Creates a hook to be added to an instance of logger. This is called with
+// `hook, err := NewHttpHook("http://log-server/post_new_log", "logBody")`
+// `if err == nil { log.Hooks.Add(hook) }`
+func NewHttpHook(endpoint string, formKey string, extraFields map[string]string) (*HttpHook, error) {
+  return &HttpHook{endpoint, formKey, extraFields}, nil
+}
+
+func (hook *HttpHook) Fire(entry *logrus.Entry) error {
+  line, err := entry.String()
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "Unable to read entry, %v", err)
+    return err
+  }
+
+  reqForm := url.Values{}
+
+  // add in extra fields, if any
+  for k, v := range hook.RequestExtraFields {
+    reqForm.Set(k, v)
+  }
+
+  // add log line
+  reqForm.Set(hook.RequestFormKey, line)
+
+  _, err = http.PostForm(hook.RequestEndpoint, reqForm)
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "Unable to post line to server, %v", err)
+    return err
+  }
+
+  return nil
+}
+
+func (hook *HttpHook) Levels() []logrus.Level {
+  return logrus.AllLevels
+}
